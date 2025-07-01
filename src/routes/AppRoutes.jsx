1e6150f1d67cdd43bea02ad1205c2d467f59ac53
Sushil <grayson.9999@gmail.com>
2025-07-02 09:29:38 +1000

    starting authentication system, routing completed

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import DefaultLayout from "../components/layouts/DefaultLayout";
+import SignInPage from "../pages/auth/SignInPage";
+import SignUpPage from "../pages/auth/SignUpPage";
+import UserLayout from "../components/layouts/UserLayout";
+import HomePage from "../pages/home/HomePage";
+import DashboardPage from "../pages/dashboard/DashboardPage";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      {/* default routing */}
+      <Route path="/" element={<DefaultLayout />}>
+        <Route index element={<HomePage />}></Route>
+        <Route path="login" element={<SignInPage />}></Route>
+        <Route path="signup" element={<SignUpPage />}></Route>
+      </Route>
+      {/* private routing  */}
+      <Route path="/user" element={<UserLayout />}>
+        <Route index element={<DashboardPage />}></Route>
+      </Route>
+    </Routes>
+  );
+};
+
+export default AppRoutes;
