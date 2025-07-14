7583984e52173b1f9688e38584d5c71a236538be
Sushil <grayson.9999@gmail.com>
2025-07-14 19:34:26 +1000

    implementing custom hook

--- src/pages/auth/SignUpPage.jsx
+++ src/pages/auth/SignUpPage.jsx
@@ -3,8 +3,15 @@ import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
 import CustomInput from "../../components/customInput/CustomInput";
 import { signUpInputs } from "../../assets/customInputs/userSignUpInput";
+import useForm from "../../hooks/useForm";
 
+const initialState = {};
 const SignUpPage = () => {
+  const { form, setForm, handleOnChange } = useForm(initialState);
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
   return (
     <div className="form-wrapper bg-body-tertiary m-1 d-flex justify-content-center align-items-center flex-column">
       <div className="text-center">
@@ -14,9 +21,13 @@ const SignUpPage = () => {
       </div>
 
       <div className="form-content">
-        <Form className="form p-4 shadow-lg">
+        <Form onSubmit={handleOnSubmit} className="form p-4 shadow-lg">
           {signUpInputs.map((input) => (
-            <CustomInput key={input.name} {...input} />
+            <CustomInput
+              key={input.name}
+              {...input}
+              onChange={handleOnChange}
+            />
           ))}
 
           <Button className="w-100" variant="primary" type="submit">
